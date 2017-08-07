71d5dd2c857311fff11e2437ee690d1d766c8035
Heather Rae <heatherrae150@yahoo.co.uk>
2017-08-07 10:25:45 +0100

    Add example answers after homework review

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,36 +1,71 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+#answer
+# def pet_shop_name(pet_shop)
+#   return pet_shop[:name]
+# end
 
 
 
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+# answer
+# def pets_in_stock(pet_shop)
+#   return pet_shop[:pets]
+# end
+
+# def total_cash(pet_shop)
+#   cash = admin(pet_shop)[:total_cash]
+#   return cash
+# end
+
 
 
 
 def add_or_remove_cash(pet_shop, money)
   return pet_shop[:admin][:total_cash] += money
 end
+# answer
+# def add_or_remove_cash(pet_shop,value)
+#   admin(pet_shop)[:total_cash] += value
+# end
+
 
 
 
 def pets_sold(pet_shop)
   pet_shop[:admin][:pets_sold]
 end
+# answer
+# def admin(pet_shop)
+#   pet_shop[:admin]
+# end
+
+# def pets_sold(pet_shop)
+#   return admin(pet_shop)[:pets_sold]
+# end
 
 
 
 def increase_pets_sold(pet_shop, sales)
   return pet_shop[:admin][:pets_sold] += sales
 end
+# answer
+# def increase_pets_sold(pet_shop, value)
+#   admin(pet_shop)[:pets_sold] += value
+# end
 
 
 
 def stock_count(pet_shop)
   return pet_shop[:pets].length()
 end
+# answer
+# def stock_count(pet_shop)
+#   return pets_in_stock(pet_shop).count
+# end
 
 
 
@@ -47,6 +82,14 @@ count = []
   return count
 
 end
+# answer
+# def pets_by_breed(pet_shop, breed)
+#   matches = []
+#   for pet in pets_in_stock(pet_shop)
+#     matches << pet if(pet[:breed] == breed)
+#   end
+#   return matches
+# end
 
 
 
@@ -62,6 +105,14 @@ def find_pet_by_name(pet_shop, name)
   return nil
 
 end
+# answer
+# def find_pet_by_name(pet_shop, pet_name)
+#   match = nil
+#   for pet in pets_in_stock(pet_shop)
+#     match = pet if(pet[:name] == pet_name)
+#   end
+#   return match
+# end
 
 
 
@@ -76,18 +127,43 @@ def remove_pet_by_name(pet_shop, name)
   end
 
 end
+# answer
+# def remove_pet_by_name(pet_shop, pet_name)
+#   pets = pets_in_stock(pet_shop)
+#   index = 0
+#   match = nil
+#
+#   for pet in pets
+#     if(pet[:name] == pet_name)
+#       match = pet
+#       break
+#     end
+#     index += 1
+#   end
+#
+# return if(match == nil)
+#   pets.delete_at(index)
+# end
 
 
 
 def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets].push(new_pet)
 end
+# answer
+# def add_pet_to_stock(pet_shop, pet)
+#   pets_in_stock(pet_shop).push(pet)
+# end
 
 
 
 def customer_pet_count(customer)
   return customer[:pets].length()
 end
+# answer
+# def customer_pet_count(customer)
+#   return customer[:pets].count
+# end
 
 
 
@@ -95,6 +171,10 @@ def add_pet_to_customer(customer, new_pet)
   customer[:pets].push(new_pet)
   return customer[:pets].length()
 end
+# answer
+# def add_pet_to_customer(customer, pet)
+#   customer[:pets] << pet
+# end
 
 
 
@@ -105,6 +185,10 @@ def customer_can_afford_pet(customer, new_pet)
     return false
   end
 end
+# answer
+# def customer_can_afford_pet(customer, pet)
+#   return customer[:cash] >= pet[:price]
+# end
 
 
 def sell_pet_to_customer(pet_shop, pet, customer)
@@ -125,3 +209,13 @@ def sell_pet_to_customer(pet_shop, pet, customer)
     customer[:cash] -= pet[:price]
   end
 end
+# answer
+# def sell_pet_to_customer(pet_shop, pet, customer)
+#   return if (pet == nil)
+#   return if !(customer_can_afford_pet(customer, pet))
+#
+#   add_pet_to_customer(customer,pet)
+#   add_or_remove_cash(pet_shop, pet[:price])
+#   remove_pet_by_name(pet_shop, pet[:name])
+#   increase_pets_sold(pet_shop,1)
+# end
